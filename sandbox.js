6005a0721cd6209cd0a1b860ada30715e5ce104a
miyushan <smiushan2015@gmail.com>
2021-04-22 12:07:14 -0700

    boolean

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,9 @@
-let age = null;
+//console.log(true, false, 'true', 'false');
 
-console.log(age, age+3, `the age is ${age}`);
+let email = 'smiushan2015@gmail.com';
+let result = email.includes('!');
+console.log(result);
+
+let age = 23;
+console.log(age == 23);
+console.log(age == 30);
