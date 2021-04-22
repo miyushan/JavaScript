b73ddd6045077826b8fdf29ddde41d56d25885bf
miyushan <smiushan2015@gmail.com>
2021-04-22 10:10:36 -0700

    about string part 2

--- sandbox.js
+++ sandbox.js
@@ -20,3 +20,13 @@ console.log(fullName.length);
 console.log(fullName.toUpperCase());
 let result = fullName.toLowerCase();
 console.log(result);
+
+let index = food.indexOf('c');
+console.log(index);
+
+let email = 'smiushan2015@gmail.com';
+//let index1 = email.lastIndexOf('m');
+//let index2 = email.slice(0,10);
+//let index3 = email.substr(4,10);
+let index4 = email.replace('m','x');
+console.log(index4);
