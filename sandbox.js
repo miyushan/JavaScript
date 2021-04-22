1c303342504fb8c77e3862535d86f4d1de7ced0e
miyushan <smiushan2015@gmail.com>
2021-04-22 10:52:11 -0700

    concatenation

--- sandbox.js
+++ sandbox.js
@@ -1,32 +1,8 @@
-//strings
-console.log.apply('Hello UOJ');
+let title = "com";
+let author = "miyushan";
+let age = 23;
 
-let food = 'rice';
-console.log(food);
+//let result = "I am studing " + title + ". And my name is " + author + ". My age is " + age;
 
-//string concatenation
-let firstName = 'Shakeer';
-let LastName = 'Miyushan';
-let fullName = firstName + ' ' + LastName;
-console.log(fullName);
-
-//getting charactors
-console.log(fullName[4]);
-
-//get length
-console.log(fullName.length);
-
-//string methods
-console.log(fullName.toUpperCase());
-let result = fullName.toLowerCase();
+let result = `I am studing ${title}. And my name is ${author}. My age is ${age}.`
 console.log(result);
-
-let index = food.indexOf('c');
-console.log(index);
-
-let email = 'smiushan2015@gmail.com';
-//let index1 = email.lastIndexOf('m');
-//let index2 = email.slice(0,10);
-//let index3 = email.substr(4,10);
-let index4 = email.replace('m','x');
-console.log(index4);
