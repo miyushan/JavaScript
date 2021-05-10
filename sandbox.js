26b1694a62ad9b9725f765c8d5f4adb41de2cc3d
miyushan <smiushan2015@gmail.com>
2021-05-10 15:56:25 +0530

    methods

--- sandbox.js
+++ sandbox.js
@@ -1,41 +1,11 @@
-//normal function
-const area = function(radius){
-    let temp = Math.PI*radius*radius;
-    return temp;
-}
+const name = 'shan';
 
-//arrow function
-const arrowFunArea = (radius) => {
-    let temp = Math.PI*radius*radius;
-    return temp;
-};
+//function
+const great = () => 'hello';
 
-//more shorter
-const shortArea = radius => Math.PI*radius**2;
+let resultOne = great();
+console.log(resultOne);
 
-const globleArea = area(7);
-console.log(`Area is: ${globleArea}`);
-
-const globleArrowArea = arrowFunArea(7);
-console.log(`Area is: ${globleArrowArea}`);
-
-const globleshortArea = shortArea(7);
-console.log(`Area is: ${globleArrowArea}`);
-
-
-
-
-//examples
-const great = () => 'hello, world';
-const global = great();
-console.log(global);
-
-const bill = (products, tax) => {
-    let tot = 0;
-    for(let i = 0; i<products.length; i++){
-        tot += products[i] + products[i]*tax;
-    }
-    return tot;
-};
-const ans = bill([10,15,30],0.2);
-console.log(ans);
+//method
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
