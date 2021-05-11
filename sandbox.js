49e7fd04c5220775359453ec2f65b4dd2206edb1
miyushan <smiushan2015@gmail.com>
2021-05-11 18:13:54 +0530

    find elements by deferent selectors

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,9 @@
-const more = document.querySelectorAll('p');
-console.log(more);
+const get = document.getElementById('pageTitle');
+console.log(get);
+
+const get2 = document.getElementsByClassName('error');
+console.log(get2);
+
+const get3 = document.getElementsByTagName('p');
+console.log(get3);
+console.log(get3[2]);
