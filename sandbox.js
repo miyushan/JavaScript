c704225407c950626f6eec609196d40bb4e74091
miyushan <smiushan2015@gmail.com>
2021-05-11 17:16:45 +0530

    Math operators

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,5 @@
-let user = {
-    name: 'miyushan',
-    age: 30,
-    email: 'smiushan2015@gmail.com',
-    blogs: [
-        { title: 'my name is miyushan', likes: 30},
-        { title: 'my name is shakeer', likes: 20}
-    ],
-    login: function(){
-        console.log('user loged in');
-    },
-    logout: () => {
-        console.log('user loged out');
-    },
-    logBlogs (){
-         console.log('this user wrote these things');
-         this.blogs.forEach( blog =>{
-             console.log(blog.title, blog.likes);
-         })
-    }
-};
+console.log(Math);
+console.log(Math.PI);
 
-// user.login();
-// user.logout();
-
-user.logBlogs();
+const random = Math.random();
+console.log(Math.round(random * 100));
