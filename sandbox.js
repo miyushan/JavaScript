9e6d5067dc2ec155421d6f77661497f8136c736d
miyushan <smiushan2015@gmail.com>
2021-05-11 19:49:49 +0530

    read text inside tags and add classes for them

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,10 @@
-const temp = document.querySelector('h1');
-temp.classList.add('error');
-//temp.classList.remove('error');
+const temp = document.querySelectorAll('p');
+
+temp.forEach(obj => {
+    if(obj.textContent.includes('error')){
+        obj.classList.add('error');
+    }
+    if(obj.textContent.includes('success')){
+        obj.classList.add('success');
+    }
+})
