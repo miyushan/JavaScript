557552003cbf449ee77ae5ddb97e87306ce6aa19
miyushan <smiushan2015@gmail.com>
2021-05-11 19:21:16 +0530

    change attributes by dot operator

--- sandbox.js
+++ sandbox.js
@@ -2,4 +2,7 @@ const content = document.querySelector('.main_headline');
 
 console.log(content.getAttribute('main_headline'));
 content.setAttribute('class','newClass');
-content.setAttribute('style','color:yellow');
+content.setAttribute('style','color: yellow');
+
+content.style.margin = '50px';
+content.style.fontSize = '200px';
