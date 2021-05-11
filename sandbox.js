1ca9572a26471bfc760e762bbb07ff8a3152d356
miyushan <smiushan2015@gmail.com>
2021-05-11 19:36:52 +0530

    add and remove classes

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,3 @@
-const content = document.querySelector('.main_headline'); 
-
-console.log(content.getAttribute('main_headline'));
-content.setAttribute('class','newClass');
-content.setAttribute('style','color: yellow');
-
-content.style.margin = '50px';
-content.style.fontSize = '200px';
+const temp = document.querySelector('h1');
+temp.classList.add('error');
+//temp.classList.remove('error');
